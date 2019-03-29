3861b4dfcbaa4d98b825999348c81fc84ff4a251
jbolaty <jeanmarcbolaty@yahoo.fr>
2019-03-29 11:23:01 +0100

    Modification des thèmes et contenu des posts

--- my-blog/src/app/app.component.ts
+++ my-blog/src/app/app.component.ts
@@ -11,24 +11,23 @@ export class AppComponent {
 
   posts= [
     {
-    title: 'Mon premier post',
-    content: "contenu1",
+    title: 'Frameworks',
+    content: "On se pose toujours la question de savoir lequel de React, Angular et VueJs est le meilleur framework. A mon avis je dirais Angular et vous ne me ferez pas changer d'avis",
     loveIts: 0,
     created_at: this.postDate
   },
   {
-    title: 'Mon deuxième post',
-    content: "contenu2",
+    title: 'Jeux Video',
+    content: "FIFA2019 est le meilleur jeu de sport sur le marché. Ceci reste un avis personnel ! ",
     loveIts: 0,
     created_at: this.postDate
   },
   {
-    title: 'Encore un post',
-    content: "contenu 3",
+    title: 'Musique',
+    content: "hum ... En terme de style musicale je trouve que le rap est un peu agressif. Je préfère planner avec du bon reggae oh yeahh !!",
     loveIts: 0,
     created_at: this.postDate
   },]
-
 
 
 }
